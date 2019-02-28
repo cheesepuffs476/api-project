84fed65eb833b59d017885e3764b06c68e82773e
concon2015 <connorblackard@gmail.com>
2019-02-28 15:41:14 -0600

    Added dockerfile and requirments.txt

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
+# Import flask
 from flask import Flask
 
+# Setup your app
 app = Flask(__name__)
 
+# Set up a route
 @app.route('/')
-def helloworld():
-    return "Hello World"
+def index():
+    # Do cool shit here
+    return 'it works'
 
-app.debug = True
-app.run()
+# More routes go here...
+
+# This check will only run the code if you run it from the terminal, not if you import it
+if __name__ == '__main__':
+    # Set the debug = true
+    app.debug = True
+    # Run the app
+    app.run('0.0.0.0')
