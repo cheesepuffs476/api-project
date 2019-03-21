1af28820fce4d86754ecfc04fdb7a05ede0c8a80
TBoiles18 <47221989+TBoiles18@users.noreply.github.com>
2019-03-21 11:15:45 -0500

    Update main.py
    Fibonacci update, jeffery look at this to confirm

--- main.py
+++ main.py
@@ -41,6 +41,30 @@ def send_to_slack(message):
 #           return ethelse
 #    )
 @app.route('/fibonacci/<int:userInput>')
+
+
+
+
+
+nterms = int(input("How many terms? "))
+n1 = 0
+n2 = 1
+count = 0
+if nterms <= 0:
+   print("Please enter a positive integer")
+elif nterms == 1:
+   print("Fibonacci sequence upto",nterms,":")
+   print(n1)
+else:
+   print("Fibonacci sequence upto",nterms,":")
+   while count < nterms:
+       print(n1,end=' , ')
+       nth = n1 + n2
+       n1 = n2
+       n2 = nth
+       count += 1
+
+
 def hashfunction(userInput):
     hash_object = hashlib.md5(userInput.encode())
     return jsonify(
