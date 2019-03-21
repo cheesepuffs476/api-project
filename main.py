b24751d0a6c899413d0e7c87966fe8c9b2c322cf
TBoiles18 <47221989+TBoiles18@users.noreply.github.com>
2019-03-21 12:05:23 -0500

    Update main.py
    improved fib function

--- main.py
+++ main.py
@@ -40,29 +40,18 @@ def send_to_slack(message):
 #           ethelse = userInput(userInput-1)
 #           return ethelse
 #    )
-@app.route('/fibonacci/<int:userInput>')
-
-
-
-
-
-nterms = int(input("How many terms? "))
-n1 = 0
-n2 = 1
-count = 0
-if nterms <= 0:
-   print("Please enter a positive integer")
-elif nterms == 1:
-   print("Fibonacci sequence upto",nterms,":")
-   print(n1)
-else:
-   print("Fibonacci sequence upto",nterms,":")
-   while count < nterms:
-       print(n1,end=' , ')
-       nth = n1 + n2
-       n1 = n2
-       n2 = nth
-       count += 1
+def fibonaccifunction(userInput):
+   results=[1,1]
+   a=1
+   while(a<userInput):
+       a=a+results[-2]
+       if (a<userInput):
+           results.append(a)
+   return jsonify(
+       input=userInput,
+       output=results
+   )
+   
 
 @app.route('/is-prime/<int:userInput>')
 def is_prime(userInput):
