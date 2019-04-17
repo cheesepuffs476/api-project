200ad8ba80cc77d7ec450986061d597d5738386a
Nguyetcam Truong <ngtruong@tamu.edu>
2019-04-17 04:44:11 +0000

    comments

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
-# Import flask
+#Import flask
 from flask import Flask, jsonify
 import hashlib
 import requests
 import json
 from flask import Flask, request
 
-# Setup your app
+#Setup app
 slack_token = 'https://hooks.slack.com/services/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'
 app = Flask(__name__)
 
-# Set up a route
+#Set up route
 @app.route('/')
 def index():
     # Do cool shit here
     return 'it works'
 
-# More routes go here...
+#Additional routes
 @app.route('/md5/<string:userInput>')
 def hashfunction(userInput):
     hash_object = hashlib.md5(userInput.encode())
@@ -24,28 +24,26 @@ def hashfunction(userInput):
         output=hash_object.hexdigest()
     )
 
-#Slack Alert
+#Slack Alerts
 @app.route('/slack-alert/<string:message>')
 def send_to_slack(message):
     payload = {'text': message}
     requests.post(slack_token, json.dumps(payload))
     return 'True'
 
-#@app.route('/factorial/<string:userInput')
-#def factorialfunction(userInput):
-#    if userInput==1 :
-#                  return userInput
-#     else :
-#
-#           ethelse = userInput(userInput-1)
-#           return ethelse
-#    )
+#Endpoint returns factorial for integer that is inputted. If integer is not positive, error message will be displayed
+@app.route('/factorial/<string:userInput')
+def factorialfunction(userInput):
+    if userInput==1 :
+                  return userInput
+     else :
+
+           ethelse = userInput(userInput-1)
+           return ethelse
+    )
+
+#An array of integers with all Fibonacci numbers less than or equal to the input  will be outputted. If the integer is not positive, error message will be displayed
 @app.route('/fibonacci/<int:userInput>')
-
-
-
-
-
 nterms = int(input("How many terms? "))
 n1 = 0
 n2 = 1
@@ -64,27 +62,36 @@ else:
        n2 = nth
        count += 1
 
+#A boolean value will be returned depending on whether the input is a prime number. If the input is invalid, error message will be displayed
 @app.route('/is-prime/<int:userInput>')
 def is_prime(userInput):
-    try:
-        userInput += 1
-    except TypeError:
-        return jsonify(
-            input = userInput,
-            error = "invalid input, input must be an int"
-        )
-    if userInput >= 2:
-        for x in range(2,userInput):
-            #if divisible, return false
-            if not(userInput % x):
-                return False
+    if isinstance(userInput,int):
+        prime = True
+        if userInput < 2:
+            prime = False
+            return jsonify(
+                input= userInput,
+                output= 'Input is not prime'
+            )
+        else:
+            for n in range(2, userInput):
+                if userInput % n == 0:
+                    prime = False
+                    break
+                else:
+                    prime = True
+                    return jsonify(
+                        input= userInput,
+                        output= 'Input is prime'
+                    )
     else:
-        return False
-    return True
+        return jsonify(
+            input= userInput,
+            output= 'Input must be integer'
+        )
+            # divisible, return
 
-
-
-# This check will only run the code if you run it from the terminal, not if you import it
+# This check will only run the code if you run it from the terminal, not if it is imported
 if __name__ == '__main__':
     # Set the debug = true
     app.debug = True
