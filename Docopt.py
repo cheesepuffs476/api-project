d7eb05275e5925e9e09aad0078946aebb7275242
wnguyen142 <wnguyen142@tamu.edu>
2019-04-23 12:38:47 -0500

    Create Docopt.py

---
+++ Docopt.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+usage = """Usage:
+        test.py -h
+        test.py md5 <input>
+        test.py factorial <input>
+        test.py fibonacci <input>
+        test.py is-prime <input>
+        test.py kv-retrieve <input> 
+        test.py kv-record <input> <value>
+        test.py slack-alert <input>
+"""
+
+from docopt import docopt
+import requests
+
+args = docopt(usage)
+
+if args['md5']==True:
+    r = requests.get("http://api.blackard.org:5000/md5/"+ args["<input>"])
+    data = r.json()
+    print(data['output'])
+if args['factorial']==True:
+    r = requests.get("http://api.blackard.org:5000/factorial/"+ args["<input>"])
+    data = r.json()
+    print(data['output'])
+if args['fibonacci']==True:
+    r = requests.get("http://api.blackard.org:5000/fibonacci/"+ args["<input>"])
+    data = r.json()
+    print(data['output'])
+if args['is-prime']==True:
+    r = requests.get("http://api.blackard.org:5000/is-prime/"+ args["<input>"])
+    data = r.json()
+    print(data['output'])  
+if args['kv-retrieve']==True:
+    r = requests.get("http://api.blackard.org:5000/kv-retrieve/"+ args["<input>"])
+    data = r.json()
+    print(data['output']) 
+if args['kv-record']==True:
+    r = requests.get("http://api.blackard.org:5000/kv-record/"+ args["<input>"])
+    data = r.json()
+    print(data['output']) 
+if args['slack-alert']==True:
+    r = requests.get("http://api.blackard.org:5000/slack-alert/"+ args["<input>"])
+    data = r.json()
+    print(data['output'])
